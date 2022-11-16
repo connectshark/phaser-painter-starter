7f5fde21104a63a416c82bc1ca993dccdadccf0d
Chambers <46649021+connectshark@users.noreply.github.com>
2022-11-16 11:44:43 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view></router-view>
+</template>
+
+
+<style lang="scss">
+#app {
+  font-family: 'Poppins', 'Noto Sans TC', sans-serif;
+  text-align: center;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import { defineStore } from 'pinia'
+
+export const useDefaultStore = defineStore('store',{
+  state: () => ({
+  })
+})
